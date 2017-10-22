c5d4f20ce88b47504d50a1843ba567e0dd566117
Your Name <you@example.com>
2017-10-21 22:22:59 -0700

    added instructions file and bamazonCustomer.js with instructions commented out

---
+++ bamazonCustomer.js
@@ -0,0 +1,15 @@
+//file should first display bamazon_db information--ids, names, and prices
+
+
+//then prompts the user the for id of the product they would like to buy
+
+//then prompts the user for the quantity they would like to buy
+
+//then checks to see if there is enough of the product in stock
+
+//if not enough in stock, console.log("Insufficient quantity!"); and prevent the order from going through
+
+//if there is enough in stock, then the order should be completed
+//then the database should be updated
+
+//the user should be notified of the total price 
